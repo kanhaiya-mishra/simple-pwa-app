dc7c941d805f4eda186b3d251f19c8f66430fac0
Kanhaiya Mishra <starklm1402@gmail.com>
2021-05-24 17:21:13 +0530

    Adding dynamic caching functionality to App

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic';
+
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-        caches.open('static')
+        caches.open(CACHE_STATIC_NAME)
             .then((cache) => {
                 console.log('[Service Worker] PreCaching app shell ....');
                 cache.addAll([
@@ -19,6 +22,17 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ....', event);
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(cacheNames.map((key => {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('[Service Worker] Deleting old cache', key);
+                        return caches.delete(key);
+                    }
+                })));
+            })
+    )
     return self.clients.claim();
 });
 
@@ -29,7 +43,15 @@ self.addEventListener('fetch', function (event) {
                 if (response) {
                     return response;
                 }
-                return fetch(event.request);
+                return fetch(event.request)
+                    .then((res) => {
+                        return caches.open(CACHE_DYNAMIC_NAME)
+                            .then((cache) => {
+                                cache.put(event.request.url, res.clone());
+                                return res;
+                            })
+                    })
+                    .catch(() => { });
             })
     );
 });
